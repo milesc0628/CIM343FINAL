f6dd9a8b7da7ca7058c08e3959caea21a9c1be15
milesc0628 <miles.ec888@gmail.com>
2026-05-02 22:43:51 +0000

    updae

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 
@@ -50,7 +51,24 @@ async function syncDB(){
 
 syncDB().catch(console.error);
 
-const url = 'https://api.themoviedb.org/3/authentication';
+const List = sequelize.define('List', {
+  name: { type: DataTypes.STRING, allowNull: false },
+});
+
+const Task = sequelize.define('Task',{
+  name:{type: DataTypes.STRING,allowNull:false},
+  description:{type: DataTypes.TEXT}
+});
+
+// 1. A List can contain multiple Tasks
+List.hasMany(Task, {
+  onDelete: 'CASCADE'   // If a List is deleted, delete its Tasks
+});
+
+// 2. A Task belongs to a single List
+Task.belongsTo(List);
+
+/* const url = 'https://api.themoviedb.org/3/authentication';
 const options = {
   method: 'GET',
   headers: {
@@ -62,14 +80,14 @@ const options = {
 fetch(url, options)
   .then(res => res.json())
   .then(json => console.log(json))
-  .catch(err => console.error(err));
+  .catch(err => console.error(err)); */
 
 /* GET home page. */
-app.get('/', function (req, res, next) {
+ app.get('/', function (req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
-app.get('/landing', function (req, res, next) {
+/*app.get('/landing', function (req, res, next) {
   res.render('landing', { title: 'Home' });
 });
 
@@ -79,8 +97,38 @@ app.get('/movies', function (req, res, next) {
 
 app.get('/tv', function (req, res, next) {
   res.render('tv', {title:'tv'});
+}); */
+
+app.get('/addtask',function(req,res,next){
+  res.render('addtask',{title:'Add Task'});
 });
 
+app.post('/addtask', async function(req,res,next){
+  try{
+    const created = await Task.create({name:req.body.name, description: req.body.description});
+    res.json(created);
+  }catch(err){
+    next(err);
+  }
+});
+
+app.get('/addlist', function (req, res, next) {
+  res.render('addlist', { title: 'Add List' });
+});
+
+app.post('/addlist', async function (req, res, next) {
+  console.log('Received addlist POST:', req.body);
+  try {
+        // req.body is now the clean JSON object
+        const list = await List.create(req.body, {
+            include: [Task]
+        });
+        res.json(list);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: err.message });
+    }
+});
 
 
 
